b0bd5356c13224a9f609cc8700c9631f00ddcfa9
mizucode <mizurisysthensis3@gmail.com>
2023-05-18 10:31:17 +0700

    fix responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
 		},
 		extend: {
 			backgroundImage: {
-				hero: "url('/img/hanabi.jpg')",
+				hero: "url('/img/hanabi-01.png')",
 			},
 			fontFamily: {
 				montserrat: ["Montserrat"],
